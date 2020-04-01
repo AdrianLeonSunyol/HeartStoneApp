1655bc658984cc63e78e3a9a9f55ca9d93f9b85b
AdrianLeonSunyol <aals1@alu.ua.es>
2020-04-01 15:04:36 +0200

    add cards component and adapted navigation

---
+++ src/app/card-decks/card-decks.module.ts
@@ -0,0 +1,25 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { IonicModule } from '@ionic/angular';
+import { RouterModule } from '@angular/router';
+import { CardDecksComponent } from "./card-decks.component";
+
+@NgModule({
+  imports: [
+    CommonModule,
+    FormsModule,
+    IonicModule,
+    RouterModule.forChild([{ path: '', component: CardDecksComponent }])
+  ],
+  declarations: [CardDecksComponent],
+})
+export class CardDecksModule {
+  readonly cardDecks: string [] = [
+    `Druid`,
+    `Mage`,
+    `Warrior`,
+    `Rogue`,
+    `Shaman`
+  ];
+ }
